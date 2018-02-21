fbc8048716f2436e445529fb27e0185616bb3784
Torben Hartmann <th@stadesign.de>
2018-02-21 13:02:45 +0100

    Multi window + Debug

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const queueTypeId = process.argv[2];
+
+
 if (queueTypeId === undefined) {
     console.error("Queue channel argument missing!");
     console.error("\t try running the process:");
@@ -6,8 +8,13 @@ if (queueTypeId === undefined) {
     process.exit(-1);
 }
 
-const queue = require("./lib/messageQueue")(1337, parseInt(queueTypeId));
 const socket = require("./lib/socketConnection")(8080);
+global.debug = (message, ...args) => {
+    socket.send("debug", message + ":\n" + JSON.stringify(args));
+    console.log(message + ":\n" + JSON.stringify(args));
+};
+
+const queue = require("./lib/messageQueue")(1337, parseInt(queueTypeId));
 
 const StateStack = require("./lib/stateStack");
 const stack = new StateStack({
@@ -23,6 +30,9 @@ const stack = new StateStack({
 });
 
 const eventHandler = (arg) => stack.processEvent(arg);
-queue.on("event", eventHandler);
-socket.on("remote", eventHandler);
-queue.start();
+queue.on("event", (...arg) => eventHandler(...arg));
+socket.on("remote", (...arg) => {
+    debug("REMOTE Event", arg);
+    eventHandler(...arg);
+});
+//queue.start();
