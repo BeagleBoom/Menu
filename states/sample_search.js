aab45db6006fcfac47b3711ec5c635eab1ab4641
Torben Hartmann <th@stadesign.de>
2018-02-23 13:12:53 +0100

    Ecma 5 :-/

--- states/sample_search.js
+++ states/sample_search.js
@@ -1,3 +1,46 @@
+let request = require('then-request');
+let querystring = require("querystring");
+
+const search = (text, startPage = 1) => {
+
+    let oAuth = {token: {access_token: "sEzGLDzLDtuzUW8itzDPPYek0NjYmD", "token_type": "Bearer"}};
+
+    // Fields and Filters
+    let fields = 'id,name,url,tags,description,duration,avg_rating,license,type,channels,filesize,bitrate,samplerate,username,pack,num_downloads,avg_ratings,num_ratings';
+
+    // Request Parameters
+    let headers = {Authorization: oAuth.token.token_type + " " + oAuth.token.access_token};
+    let uri = "https://www.freesound.org/apiv2/search/text/?" + querystring.stringify({
+        query: text,
+        page: startPage,
+        fields: fields
+    });
+
+    // Search for Text
+    return request('GET', uri, {headers: headers})
+        .getBody('utf8')
+        .then(JSON.parse)
+        .then(result => {
+            result.results = result.results.map(r => {
+                r.samplerate = Math.round(r.samplerate / 1000) + " kb/s";
+
+                r.mono = r.channels < 2;
+                r.avg_rating = Math.round(r.avg_rating * 100) / 100;
+
+                r.duration = [
+                    Math.floor(r.duration / 60) % 60,
+                    Math.ceil((r.duration % 60) * 100) / 100
+                ]
+                    .map(d => d < 10 ? "0" + d : "" + d)
+                    .join(":");
+                return r;
+            });
+            return result;
+        })
+        ;
+};
+
+
 module.exports = ({Arg0, Else}, api) => {
     return {
         name: "sample_search",
@@ -13,8 +56,12 @@ module.exports = ({Arg0, Else}, api) => {
                 api.send("WAITING", "Waiting for search results.");
             }
         }, start: (name) => {
-            api.pushState("_keyboard");
-            console.log("Starting search")
+            search("storm").then(result => {
+                console.log(result);
+                result.results[1].active = true;
+                api.display("sound_list", result);
+            })
+
         },
         events: {
             "FREESOUND_SEARCHRESULTS": [
