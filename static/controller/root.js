b1db71716ab9fb4fa99a12a939fc306586767695
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2018-06-29 16:42:47 +0200

    test fixed O:-)

--- static/controller/root.js
+++ static/controller/root.js
@@ -1,6 +1,12 @@
 function $root() {
     return function () {
         return {
+            resume: function (data) {
+                document.querySelectorAll('.Drow').forEach(function (elem) {
+                    elem.style.visibility = 'visible'
+                });
+            },
+
             start: function (data) {
                 document.querySelectorAll('.Drow').forEach(function (elem) {
                     elem.style.visibility = 'visible'
