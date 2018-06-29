6d14cf41caf4b7129190bbaba5ca064897b70c74
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2018-06-29 16:49:20 +0200

    testi undone

--- static/controller/root.js
+++ static/controller/root.js
@@ -2,11 +2,9 @@ function $root() {
     return function () {
         return {
             resume: function (data) {
-
-                document.getElementById("A").style.visibility = 'visible';
-                document.getElementById("B").style.visibility = 'visible';
-                document.getElementById("C").style.visibility = 'visible';
-                document.getElementById("D").style.visibility = 'visible';
+                document.querySelectorAll('.Drow').forEach(function (elem) {
+                    elem.style.visibility = 'visible'
+                });
             },
 
             start: function (data) {
