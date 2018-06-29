677c3538aa7e03a6c54935b9644477dd51710e00
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2018-06-29 16:47:01 +0200

    testi

--- static/controller/root.js
+++ static/controller/root.js
@@ -2,9 +2,7 @@ function $root() {
     return function () {
         return {
             resume: function (data) {
-                document.querySelectorAll('.Drow').forEach(function (elem) {
-                    elem.style.visibility = 'visible'
-                });
+
                 document.getElementById("A").style.visibility = 'visible';
                 document.getElementById("B").style.visibility = 'visible';
                 document.getElementById("C").style.visibility = 'visible';
