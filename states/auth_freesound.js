db180fa12de17c69b94d9ad4b651ff8fe646be8e
Lasse Kathke, CEO Kathke Research <lasse.kathke@student.fh-kiel.de>
2018-06-30 12:33:13 +0200

    Added HTTPS Authentification

--- states/auth_freesound.js
+++ states/auth_freesound.js
@@ -9,9 +9,9 @@ module.exports = ({Arg0, Else}, api) => {
                 api.display("auth_freesound", resp);
             });
             let timer = setInterval(async () => {
-                api.getContent(data.freesound.baseUrl + "poll?bbid=3").then((resp) => {
+                api.getContent(data.freesound.baseUrl + "poll").then((resp) => {
                     let response = JSON.parse(resp);
-                    if (response.authed) {
+                    if (response.access_token !== undefined) {
                         clearInterval(timer);
                         api.popState(null);
                     }
