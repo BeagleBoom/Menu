824dd4226b694fdfda4e25f7e75ca46261d034d3
Torben Hartmann <th@stadesign.de>
2018-06-30 14:38:17 +0200

    Midi + Auto-Reload

---
+++ states/midi.js
@@ -0,0 +1,117 @@
+const midi = require('midi');
+
+const inputs = new midi.input();
+
+const {send, start} = require("../lib/messageQueue")();
+
+start(true);
+
+let started = [];
+
+const open = (port, name) => {
+    if (port === -1 || started.includes(name)) return;
+    started.push(name);
+    const channel = new midi.input();
+    channel.on('message', function (deltaTime, message) {
+        if (selectedMidiDevice !== name) {
+            return;
+        }
+        let trigger = false;
+        let note = message[1];
+        switch (message[0]) {
+            case 0x90:
+                trigger = true;
+                break;
+            case 0x80:
+                trigger = false;
+                break;
+            default:
+                return;
+        }
+        let eventPackage = [
+            2,//CV 1
+            note,
+            6,//Gain 1
+            trigger ? 1 : 0
+        ];
+        send("ADC_VALUES", eventPackage);
+    });
+};
+
+
+let selectedMidiDevice = null;
+module.exports = ({Arg0, Else}, api) => {
+    return {
+        name: "midi",
+        title: "Midi Settings",
+        captions: {
+            "D": "Select"
+        },
+        data: {
+            devices: ["Off"],
+            selectedItem: "Off"
+        },
+        resume: (name, returnData) => {
+
+        },
+        start: (initData) => {
+            initData.devices = initData
+                .devices
+                .concat(Array
+                    .from(Array(inputs.getPortCount()))
+                    .map((v, i) => inputs.getPortName(i))
+                );
+
+            if (selectedMidiDevice == null) {
+                selectedMidiDevice = initData.selectedItem;
+            }
+            let selectedIndex = initData.devices.indexOf(selectedMidiDevice);
+            if (selectedIndex === -1) {
+                selectedIndex = 0;
+                selectedMidiDevice = "Off";
+            }
+
+            initData.selectedIndex = selectedIndex;
+            initData.currentIndex = selectedIndex;
+
+            api.display("midi", initData);
+        },
+        events: {
+            "BUTTON_UP": [
+                [Arg0("D", "Z2"), [
+                    (api, data, event) => {
+                        data.selectedIndex = data.currentIndex;
+                        selectedMidiDevice = data.devices[data.selectedIndex];
+                        api.sendView("select", data.selectedIndex);
+                        open(data.selectedIndex - 1, selectedMidiDevice);
+                    }
+                ]]
+            ],
+            "ROTARY_LEFT": [
+                [Arg0("Z2"), [
+                    (api, data, event) => {
+                        data.currentIndex--;
+
+                        if (data.currentIndex < 0) {
+                            data.currentIndex = data.devices.length - 1;
+                        }
+                        api.sendView("scroll", data.currentIndex);
+                    }
+                ]]
+            ],
+            "ROTARY_RIGHT": [
+                [Arg0("Z2"), [
+                    (api, data, event) => {
+                        data.currentIndex++;
+
+                        if (data.currentIndex > data.devices.length - 1) {
+                            data.currentIndex = 0;
+                        }
+                        api.sendView("scroll", data.currentIndex);
+                    }
+                ]]
+            ]
+        }
+    }
+}
+;
---
+++ static/controller/midi.js
@@ -0,0 +1,35 @@
+function $midi() {
+    return function () {
+        var devices = [];
+
+        function setIndex(newIndex, cls) {
+            console.log(newIndex);
+            if (document.getElementsByClassName(cls).length > 0) {
+                document.getElementsByClassName(cls)[0].classList.remove(cls);
+            }
+            document.getElementById("device-" + newIndex).classList.add(cls);
+        }
+
+
+        return {
+            start: function (data) {
+                devices = data.devices;
+                setIndex(data.currentIndex, "active");
+                setIndex(data.selectedIndex, "selected");
+            },
+            onEvent: function (event, data) {
+                console.log(data, event);
+                switch (event) {
+                    case "scroll":
+                        setIndex(data, "active");
+                        break;
+                    case "select":
+                        setIndex(data, "selected");
+                        break;
+                    default:
+                        console.log("Unkown event: ", event, data);
+                }
+            }
+        }
+    }
+}
